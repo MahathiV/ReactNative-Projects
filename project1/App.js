56438165e489e9ff8a36ab131ac50a1a5b0f18e1
Mahathi V <mahathiv14@outlook.com>
2020-08-10 22:23:26 -0500

    work in progress - implemented start timer

--- project1/App.js
+++ project1/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 
 export default class App extends React.Component {
+  
+  state = {
+    count: 0,
+    timer:null
+  }
+
+  start_timer = () => {
+    this.setState({timer: setInterval(this.increaseCount,1000)})
+  }
+  
+  increaseCount = () => {
+    let {count} = this.state;
+    count+=1;
+    this.setState ({count})
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text> Hi Mahathi ... Open up App.js to start working on your app! </Text>
+        <Button title="Start" onPress = {this.start_timer}/>
+        <Button title="Stop"/>
+        <Button title="Reset"/>
+    <Text>{Math.floor(this.state.count/60)} : {this.state.count%60}</Text>
       </View>
     );
     }
