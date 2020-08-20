970c31e9c6318ed77cad16a0f50407e6eea35a50
Mahathi V <mahathiv14@outlook.com>
2020-08-19 21:49:30 -0500

    Added styling , rearranged buttons and timer display

--- project1/App.js
+++ project1/App.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button} from 'react-native';
-import {vibrate} from './utils'
-
-
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { vibrate } from "./utils";
 
 export default class App extends React.Component {
-  
   state = {
     count: 0,
-    timer:null,
+    timer: null,
     timerRunning: null,
-    phase: "Work Phase"
-  }
+    phase: "Work Phase",
+  };
 
-  start_timer = () => {
+  startTimer = () => {
     if (!this.state.timerRunning) {
       this.setState({ timerRunning: true });
       this.setState({
@@ -21,121 +18,103 @@ export default class App extends React.Component {
       });
     }
   };
-  
+
   increaseCount = () => {
-    let {count} = this.state;
-    count+=1;
+    let { count } = this.state;
+    count += 1;
 
-    if (count === 25*60)
-    {
-      this.setState({phase: "Rest Phase"})
-      count = 0
+    if (count === 25 * 60) {
+      // To vibrate the phone after 25 minutes - Work Phase
+      this.setState({ phase: "Rest Phase" });
+      count = 0;
       vibrate();
     }
 
-    if ((count === 5*60) && (this.state.phase === "Rest Phase"))
-    {
-      this.setState({phase: "Work Phase"})
-      count = 0
+    if (count === 5 * 60 && this.state.phase === "Rest Phase") {
+      // To vibrate the phone after 5 minutes - Rest Phase
+      this.setState({ phase: "Work Phase" });
+      count = 0;
       vibrate();
     }
 
+    this.setState({ count });
+  };
 
+  stopTimer = () => {
+    this.setState({ timerRunning: false });
+    clearInterval(this.state.timer);
+    this.setState({ timer: null });
+  };
 
-    this.setState ({count})
+  restartTimer = () => {
+    this.setState({ count: 0 });
+  };
 
-
-  }
-
-  stop_timer = () => {
-    this.setState({timerRunning:false})
-    clearInterval(this.state.timer)
-    this.setState({timer: null})
-  }
-
-  restart_timer = () => {
-    this.setState({count:0});
-  }
-
-  display_seconds = () =>
-  {
-    if(this.state.count%60 === 0)
-    {
-      return '00'
+  displaySeconds = () => {
+    if (this.state.count % 60 === 0) {
+      return "00";
+    } else {
+      return `${60 - (this.state.count % 60)}`;
     }
-    else
-    {
-      return `${60 - this.state.count%60}`
-    }
-  }
+  };
 
-  display_minutes = () =>
-  {
-    if (this.state.phase === "Work Phase") 
-    {
-      if(this.state.count === 0)
-      {
-        return '25'
-        
+  displayMinutes = () => {
+    if (this.state.phase === "Work Phase") {
+      if (this.state.count === 0) {
+        return "25";
+      } else {
+        return `${24 - Math.floor(this.state.count / 60)}`;
       }
-  
-      else
-      {
-        return `${24 - Math.floor(this.state.count/60)}`
-      }
-
-    }
-
-    else
-    {
-      if(this.state.count === 0)
-      {
-        return '5'
-        
-      }
-      else
-      {
-        return `${4 - Math.floor(this.state.count/60)}`
+    } else {
+      if (this.state.count === 0) {
+        return "5";
+      } else {
+        return `${4 - Math.floor(this.state.count / 60)}`;
       }
     }
+  };
 
-    
-  }
-
-  display_timer = () => {
-    return `${this.display_minutes()} : ${this.display_seconds()}`
-  }
+  displayTimer = () => {
+    return `${this.displayMinutes()} : ${this.displaySeconds()}`;
+  };
 
   phase = () => {
-    return `${this.state.phase}`
-  }
+    return `${this.state.phase}`;
+  };
 
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.buttonWrapper}>
-          <Button title="Start" onPress = {this.start_timer}/>
-        </View>
-        <Button title="Stop" onPress = {this.stop_timer}/>
-        <Button title="Reset" onPress={this.restart_timer}/>
         <View style={styles.phaseTextWrapper}>
           <Text style={styles.phaseText}>{this.phase()}</Text>
         </View>
-        <Text>{this.display_timer()}</Text>
+        <View style={styles.timerTextWrapper}>
+        <Text style={styles.timerText}>{this.displayTimer()}</Text>
+        </View>
+
+        <View style={styles.buttonWrapper}>
+          <Button title="Start" onPress={this.startTimer} />
+        </View>
+        <View style={styles.buttonWrapper}>
+          <Button title="Stop" onPress={this.stopTimer} />
+        </View>
+        <View style={styles.buttonWrapper}>
+          <Button title="Reset" onPress={this.restartTimer} />
+        </View>
       </View>
     );
-    }
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   buttonWrapper: {
-    padding: 10,
+    padding: 5,
   },
   phaseTextWrapper: {
     height: 150,
@@ -149,7 +128,16 @@ const styles = StyleSheet.create({
   phaseText: {
     fontSize: 16,
     fontWeight: "bold",
-    // color: '#800020',
     textTransform: "uppercase",
   },
+  timerText: {
+    fontWeight: "bold",
+    fontSize: 28
+  },
+  timerTextWrapper: {
+    padding:15,
+    borderColor: "black",
+    borderWidth: 1
+
+  }
 });
